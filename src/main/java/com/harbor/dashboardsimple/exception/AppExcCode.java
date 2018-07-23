d23e2adb4d089bd0be08af8ba7012a5bb6cc272f
harbor <276446675@qq.com>
2018-07-23 17:58:38 +0800

    异常编码

---
+++ src/main/java/com/harbor/dashboardsimple/exception/AppExcCode.java
@@ -0,0 +1,20 @@
+package com.harbor.dashboardsimple.exception;
+
+/**
+ * 异常编码
+ * 
+ */
+public class AppExcCode {
+	
+    public static String SYSTEM_ERROR         = "system.error";
+    
+    public static String SYSTEM_ERROR_DESC         = "系统异常";
+    
+    public static String FORM_ERROR         = "form.error";
+
+    public static String FORM_ERROR_DESC         = "表单验证失败";
+    
+    public static String LOGIN_ERROR =  "login.error.username_or_password";
+    
+    public static String LOGIN_ERROR_DESC =  "用户名或密码错误";
+}
